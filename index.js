ea1a7468966d5ff766f752aad692b1a98af458d2
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-28 12:03:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function breakOut(array, changeValue, stopValue){
-  function change(element, changeValue, stopValue){
-    if (element === stopValue){
-      break
-    }
-    else {
-      element = changeValue;
-    }
+function change(element, changeValue, stopValue){
+  if (element === stopValue){
+    break
   }
+  else {
+    element = changeValue;
+  }
+}
+
+function breakOut(array, changeValue, stopValue){
   array.forEach(change)
 }
